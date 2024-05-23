7fa28a741db922dd5a71acdb3003bcd565c78db9
KarolinaMurzyn <karola.murzyn@gmail.com>
2024-05-23 15:29:29 +0200

    Update rozwijane_menu.js

--- js/rozwijane_menu.js
+++ js/rozwijane_menu.js
@@ -1,33 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Tutaj umieść swój kod JavaScript
-  function Rozwin(event) {
-      const dropdownMenu = document.querySelector('.menu_rozwijane');
-      const przycisk = document.querySelector('.przycisk_menu span');
-      const isOpen = dropdownMenu.classList.toggle('open');
+    function Rozwin(event) {
+        const dropdownMenu = document.querySelector('.menu_rozwijane');
+        const przycisk = document.querySelector('.przycisk_menu span');
+        const isOpen = dropdownMenu.classList.toggle('open');
+  
+        przycisk.textContent = isOpen ? '×' : '☰';
+  
+        document.querySelectorAll('.przeswit').forEach(element => {
+            element.style.display = isOpen ? 'none' : 'block';
+        });
+  
+        if (isOpen) {
+            dropdownMenu.querySelectorAll('a').forEach(link => {
+                link.addEventListener('click', handleLinkClick);
+            });
+        } else {
+            dropdownMenu.querySelectorAll('a').forEach(link => {
+                link.removeEventListener('click', handleLinkClick);
+            });
+        }
+  
+        event.preventDefault();
+    }
+  
+    function handleLinkClick(event) {
+        // Check if the link has an href attribute
+        if (!event.target.getAttribute('href')) {
+            event.stopPropagation();
+            Rozwin(event);
+        }
+    }
+  
+    document.querySelector('.przycisk_menu').addEventListener('click', Rozwin);
+  });
+  
+  
 
-      przycisk.textContent = isOpen ? '×' : '☰';
-
-      document.querySelectorAll('.przeswit').forEach(element => {
-          element.style.display = isOpen ? 'none' : 'block';
-      });
-
-      if (isOpen) {
-          dropdownMenu.querySelectorAll('a').forEach(link => {
-              link.addEventListener('click', handleLinkClick);
-          });
-      } else {
-          dropdownMenu.querySelectorAll('a').forEach(link => {
-              link.removeEventListener('click', handleLinkClick);
-          });
-      }
-
-      event.preventDefault();
-  }
-
-  function handleLinkClick(event) {
-      event.stopPropagation();
-      Rozwin(event);
-  }
-
-  document.querySelector('.przycisk_menu').addEventListener('click', Rozwin);
-});
+  
