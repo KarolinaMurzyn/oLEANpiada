69435c15b40dc9543586ed103bdfbe435a6dea35
KarolinaMurzyn <karola.murzyn@gmail.com>
2025-02-05 19:37:17 +0100

    poprawka responsywności 05.02

--- js/przesuniecie_do_sekcji.js
+++ js/przesuniecie_do_sekcji.js
@@ -2,7 +2,7 @@ function goToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
         const currentPosition = window.scrollY; // bieżąca pozycja przewijania strony
-        const targetPosition = section.offsetTop - 195; // docelowa pozycja przewijania strony
+        const targetPosition = section.offsetTop - 150; // docelowa pozycja przewijania strony
         window.scrollTo({ top: targetPosition, behavior: 'smooth' });
     }
 }
